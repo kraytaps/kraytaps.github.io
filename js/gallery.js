6d95d2dd5dcf665a12b891b494795f63e7272272
kraytaps <46850879+kraytaps@users.noreply.github.com>
2019-06-20 13:58:00 -0700

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+/*
+TILT
+*/
+$('.img-tilt').tilt({
+  scale: 1.1,
+  maxTilt: 5,
+})
+$('.gallery-logo').tilt({
+    scale: 1.1,
+    maxTilt: 5
+});
+
+$('.nav-tab').tilt({
+    scale: 1.1,
+    maxTilt: 5
+});
